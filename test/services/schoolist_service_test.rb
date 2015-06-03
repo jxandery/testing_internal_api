1889777c7646ec62ef3fefbe24c2f48a09f71980
Jack Yeh <jack.yeh@gmail.com>
2015-06-03 11:50:08 -0600

    Refactoring tests: - added setup - created instance variable @service and refactored

--- test/services/schoolist_service_test.rb
+++ test/services/schoolist_service_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class SchoolistServiceTest < ActiveSupport::TestCase
+  attr_reader :service
+
+  def setup
+    @service = SchoolistService.new
+  end
+
   test '#schools' do
     VCR.use_cassette('schoolist_service#schools') do
-      schools = SchoolistService.new.schools
+      schools = service.schools
       school = schools.first
 
       assert_equal "010601", school['uid']
@@ -14,7 +20,7 @@ class SchoolistServiceTest < ActiveSupport::TestCase
 
   test '#school' do
     VCR.use_cassette('schoolist_service#school') do
-      school = SchoolistService.new.school(10)
+      school = service.school(10)
 
       assert_equal "022101", school['uid']
       assert_equal "18.2", school['obese_percentage']
@@ -25,8 +31,8 @@ class SchoolistServiceTest < ActiveSupport::TestCase
   test '#create_school' do
     VCR.use_cassette('schoolist_service#create_school') do
       school_params = { school: { uid: "123", overweight_percentage: "12.3", obese_percentage: "19"}}
-      assert_difference("SchoolistService.new.schools.count", 1) do
-        school = SchoolistService.new.create_school(school_params)
+      assert_difference("service.schools.count", 1) do
+        school = service.create_school(school_params)
 
       assert_equal "123", school["uid"]
       assert_equal "12.3", school["overweight_percentage"]
