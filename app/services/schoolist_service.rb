3fdc471ee1b89312984052c030b6a5179e24f242
Jack Yeh <jack.yeh@gmail.com>
2015-06-03 11:37:16 -0600

    Creating tests for 2 types VCR.use_cassette functionality: Functionality 1) VCR.use_cassettes records all the SchoolistService.new.schools ojbects and returns those objects  Functionality 2) VCR.use_cassettes records and returns an individual object
    Note: Similar to a recording file:
    - SchoolistService sends an http request and records it in the yml file
    - If a second request is made it will only record the request if the yml file is empty.
    - If the yml file is not empty, it return whats is already in the yml file

--- app/services/schoolist_service.rb
+++ app/services/schoolist_service.rb
@@ -2,7 +2,7 @@ class SchoolistService
   attr_reader :connection
 
   def initialize
-    @connection = Hurley::Client.new(url: "http://localhost:3000/v1")
+    @connection = Hurley::Client.new("http://localhost:3000/v1")
   end
 
   def schools
--- test/services/schoolist_service_test.rb
+++ test/services/schoolist_service_test.rb
@@ -1,2 +1,25 @@
 require 'test_helper'
 
+class SchoolistServiceTest < ActiveSupport::TestCase
+  test '#schools' do
+    VCR.use_cassette('schoolist_service#schools') do
+      schools = SchoolistService.new.schools
+      school = schools.first
+
+      assert_equal "010601", school['uid']
+      assert_equal "18.1", school['obese_percentage']
+      assert_equal "15.9", school['overweight_percentage']
+    end
+  end
+
+  test '#school' do
+    VCR.use_cassette('schoolist_service#school') do
+      school = SchoolistService.new.school(10)
+
+      assert_equal "022101", school['uid']
+      assert_equal "18.2", school['obese_percentage']
+      assert_equal "20.8", school['overweight_percentage']
+    end
+  end
+end
+
