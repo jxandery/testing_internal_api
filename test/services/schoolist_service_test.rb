2a8ef0f33171ca891d47d68ee367b7e737767306
Jack Yeh <jack.yeh@gmail.com>
2015-06-03 12:21:23 -0600

    Created the Update Functionality for VCR.use_cassette: - update takes two parameters:   1) the object to be updated (found using the object id)   2) the information to update with
    Note: The vcr_cassette folder is automatically created every time VCR.use_cassette is run.
    - The folder needs to be manually deleted after each time its run
    - If you dont youll encounter the following error: VCR::Errors::UnhandledHTTPRequestError:

--- test/services/schoolist_service_test.rb
+++ test/services/schoolist_service_test.rb
@@ -34,12 +34,33 @@ class SchoolistServiceTest < ActiveSupport::TestCase
       assert_difference("service.schools.count", 1) do
         school = service.create_school(school_params)
 
-      assert_equal "123", school["uid"]
-      assert_equal "12.3", school["overweight_percentage"]
-      assert_equal "19.0", school["obese_percentage"]
+        assert_equal "123", school["uid"]
+        assert_equal "12.3", school["overweight_percentage"]
+        assert_equal "19.0", school["obese_percentage"]
 
-      service.destroy_school(school[:id])
+        service.destroy_school(school[:id])
       end
+    end
+  end
+
+  test '#update_school' do
+
+    VCR.use_cassette('schoolist_service#update_school') do
+      original_params = { school: { uid: "123", overweight_percentage: "32.3", obese_percentage: "14"}}
+      original_school = service.create_school(original_params)
+
+
+      updated_params = { school: { uid: "999", overweight_percentage: "12.3", obese_percentage: "19"}}
+      service.update_school(original_school['id'], updated_params)
+      updated_school = service.school(original_school['id'])
+
+      refute_equal original_school['uid'],                    updated_school['uid']
+      refute_equal original_school['overweight_percentage'],  updated_school['overweight_percentage']
+      refute_equal original_school['obese_percentage'],       updated_school['obese_percentage']
+
+      assert_equal "999", updated_school['uid'],                      updated_school['uid']
+      assert_equal "12.3", updated_school['overweight_percentage'],    updated_school['overweight_percentage']
+      assert_equal "19.0", updated_school['obese_percentage'],         updated_school['obese_percentage']
     end
   end
 end
