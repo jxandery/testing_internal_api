f3f11ca93e3e734c3eaebf88375f732c4c56e207
Jack Yeh <jack.yeh@gmail.com>
2015-06-03 11:46:39 -0600

    Created functionality for Create VCR.use_cassette.

--- test/services/schoolist_service_test.rb
+++ test/services/schoolist_service_test.rb
@@ -21,5 +21,17 @@ class SchoolistServiceTest < ActiveSupport::TestCase
       assert_equal "20.8", school['overweight_percentage']
     end
   end
-end
 
+  test '#create_school' do
+    VCR.use_cassette('schoolist_service#create_school') do
+      school_params = { school: { uid: "123", overweight_percentage: "12.3", obese_percentage: "19"}}
+      assert_difference("SchoolistService.new.schools.count", 1) do
+        school = SchoolistService.new.create_school(school_params)
+
+      assert_equal "123", school["uid"]
+      assert_equal "12.3", school["overweight_percentage"]
+      assert_equal "19.0", school["obese_percentage"]
+      end
+    end
+  end
+end
